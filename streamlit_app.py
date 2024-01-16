a271801076f99c969ddc101ebad97dffd85f44fe
PinkAlpaca0524 <156734528+PinkAlpaca0524@users.noreply.github.com>
2024-01-16 20:44:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,5 @@ streamlit.dataframe(fruits_to_show)
 
 # Dispay fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(requests.get("https://fruityvice.com/api/fruit/watermelon"))
 
