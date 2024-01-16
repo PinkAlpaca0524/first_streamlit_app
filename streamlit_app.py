eeb3cb7bf4a2843f87613395d554b3dfc94bf48b
PinkAlpaca0524 <156734528+PinkAlpaca0524@users.noreply.github.com>
2024-01-16 20:48:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 # Dispay fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
